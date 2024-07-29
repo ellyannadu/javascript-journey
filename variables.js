508aad3f759b54beef09dc314f8a10f90807a7ec
ellyannadu <duellyanna@gmail.com>
2024-07-29 23:15:56 +0800

    girl im tired

---
+++ variables.js
@@ -0,0 +1,28 @@
+console.log('hello world!');
+
+// DEFINING VARIABLES
+// the most common one is let
+let luckyNumber = 13;
+// here u set a number which is 1 of 7 primitive data types
+// (string, number, bigint, boolean, undefined, null)
+
+// u dont actually need to define it rn
+let luckyNumber;
+console.log(luckyNumber);
+// automatically set to undefined
+
+// or u can explicitly represent an empty value with null
+luckyNumber = null;
+
+// u can also set it to a string which is an entirely diff data type
+luckyNumber = 'thirteen';
+
+// any data type that isnt primitive will automatically inherit from the object class
+luckyNumber = new Object();
+
+// let isnt the only way to assign things!
+const name = 'emmanuel';
+// used for variables that cannot be assigned later!
+
+// most common way to define variables
+var badIdea = 'avoid var if unless you are smart';
